0f1dd3cc56599aab2d34546806f28b08251a7a11
nik <nikita.moiseienko@onix-systems.com>
2021-01-12 17:48:40 +0200

    feat: skeleton success

--- src/components/Task/controller.js
+++ src/components/Task/controller.js
@@ -4,11 +4,15 @@ const UserService = require('../User/service');
 
 async function editTaskView(req, res) {
     const qstrNikTokenGoalId = `?${queryString.stringify(req.query)}`;
+    console.log(req.query);
     const goal = await UserService.getGoal(req.query.nik, req.query.goalId);
     const title = `TODO | ${goal.name}`;
-    const task = await TaskModel.findOne({ _id: req.params.taskId }).exec();
+    const task = await TaskModel.findById(req.params.taskId).exec();
     const subtasksPromises = task.subtasks.map((s) => TaskModel.findById(s).exec());
-    const parents = await allTaskParents([], task._id);
+    const parents = [];
+    await allTaskParents(parents, task._id);
+    console.log(task._id);
+    console.log(parents);
     delete req.query.goalId;
     const qstrNikToken = `?${queryString.stringify(req.query)}`;
     const subtasks = await Promise.all(subtasksPromises);
@@ -25,6 +29,7 @@ async function editTaskView(req, res) {
 
 async function addSubTask(req, res) {
     const qstrNikTokenGoalId = `?${queryString.stringify(req.query)}`;
+    req.body.parent = req.params.taskId;
     req.body.owner = (await UserService.findByNik(req.query.nik))._id;
     const newTaskPromise = TaskModel.create(req.body);
     TaskModel.findByIdAndUpdate(req.params.taskId, {
@@ -35,7 +40,7 @@ async function addSubTask(req, res) {
 
 async function allTaskParents(arrayForParents, taskId) {
     const task = await TaskModel.findById(taskId).exec();
-    arrayForParents.push(task);
+    arrayForParents.unshift(task);
     if (task.parent !== null) {
         await allTaskParents(arrayForParents, task.parent);
     } else {
--- src/config/connection.js
+++ src/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const MONGODB_URI = 'mongodb://localhost:27017/';
-const MONGODB_DB_MAIN = 'todoSix';
+const MONGODB_DB_MAIN = 'todoSeven';
 const MONGO_URI = `${MONGODB_URI}${MONGODB_DB_MAIN}`;
 
 module.exports = mongoose.createConnection(MONGO_URI);
